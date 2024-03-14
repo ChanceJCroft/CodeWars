38d2affe0c14b3630403dd9917ccca6eb26d0df3
chance.croft <chance.croft@ruralsourcing.com>
2024-03-14 15:44:56 -0500

    Added an additional challenge.

--- 3-14-24.ts
+++ 3-14-24.ts
@@ -21,3 +21,36 @@
     arr = arr.sort()
     return arr[0] == arr[1] ? arr[arr.length - 1] : arr[0]
   }
+
+
+
+
+/* In this Kata, we will calculate the minimum positive number that is not a possible sum from a list of positive integers.
+
+solve([1,2,8,7]) = 4 => we can get 1, 2, 3 (from 1+2), but we cannot get 4. 4 is the minimum number not possible from the list. 
+solve([4,1,2,3,12]) = 11. We can get 1, 2, 3, 4, 4+1=5, 4+2=6,4+3=7,4+3+1=8,4+3+2=9,4+3+2+1=10. But not 11. 
+solve([2,3,2,3,4,2,12,3]) = 1. We cannot get 1.*/ 
+
+//Note - only Javascript was available for the IDE to complete this Kata
+
+
+
+
+
+  function solve(arr) {
+    //Sort the array
+    let newArr = arr.sort((a,b)=>a-b); 
+  
+    //Return 1 if possible
+    if (newArr[0]>1) return 1;
+  
+    //Check the sum against the next array item and return
+    let sum = 0;
+    for (let i=0; i<newArr.length; i++) {      
+      sum += newArr[i];
+      if (newArr[i + 1] > sum + 1) {
+        return sum + 1;
+        }
+    }
+    return sum + 1;
+}
